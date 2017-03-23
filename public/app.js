518db45c44dfaae5cbeccccb829a211c2e1eed2a
Alex M <alex.melinkhovich@favoso.de>
2017-03-23 12:02:52 +0200

    add resolve

--- public/app.js
+++ public/app.js
@@ -13,36 +13,41 @@ app.config(function($routeProvider, $locationProvider) {
         .when("/data", {
             templateUrl: "data/data.htm",
             controller: 'RouteController',
+            resolve: {
+                json: function($http, $route) {
+                    return $http.get('/json/test.json')
+                        .then(function(response) {
+                            return response.data;
+                        })
+                }
+            }
         });
     // use the HTML5 History API
     $locationProvider.html5Mode(true);
 });
-app.controller('RouteController', function($scope, $routeParams, $http) {
-    $http.get("/json/test.json")
-        .then(function(response) {
-            $scope.json = response.data;
-        });
-})
-app.controller('ExampleController', ['$scope', function ($scope) {
-        $scope.submit = function () {
-            alert('form was successfully sent!');
-        };
-        $scope.expression = function (item) {
-            // var id = item.attributes['data-clicked'].value;
-            console.log($scope.attributes);
-        };
-        $scope.doStuff = function (item) {
-            console.log(item.attributes['data-id'].value);
-            item.attributes['data-id'].value += 1;
-        };
-    }]);
+app.controller('RouteController', function($scope, json) {
+    $scope.json = json;
+});
+app.controller('ExampleController', ['$scope', function($scope) {
+    $scope.submit = function() {
+        alert('form was successfully sent!');
+    };
+    $scope.expression = function(item) {
+        // var id = item.attributes['data-clicked'].value;
+        console.log($scope.attributes);
+    };
+    $scope.doStuff = function(item) {
+        console.log(item.attributes['data-id'].value);
+        item.attributes['data-id'].value += 1;
+    };
+}]);
 
-app.directive('username', function ($q, $timeout) {
+app.directive('username', function($q, $timeout) {
     return {
         require: 'ngModel',
-        link: function (scope, elm, attrs, ctrl) {
-            ctrl.$asyncValidators.username = function (modelValue, viewValue) {
-                if (modelValue && modelValue.split(' ').length == 2) {
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$asyncValidators.username = function(modelValue, viewValue) {
+                if(modelValue && modelValue.split(' ').length == 2) {
                     return $q.resolve();
                 }
                 else {
@@ -52,37 +57,37 @@ app.directive('username', function ($q, $timeout) {
         }
     };
 });
-app.directive('popup', function () {
+app.directive('popup', function() {
     return {
         restrict: 'E',
         templateUrl: '/data/dialog.htm'
     };
 });
-app.directive("modalShow", function ($parse) {
+app.directive("modalShow", function($parse) {
     return {
         restrict: "A",
-        link: function (scope, element, attrs) {
+        link: function(scope, element, attrs) {
             //Hide or show the modal
-            scope.showModal = function (visible, elem) {
+            scope.showModal = function(visible, elem) {
 
-                if (!elem)
+                if(!elem)
                     elem = element;
 
-                if (visible)
+                if(visible)
                     $(elem).modal("show");
                 else
                     $(elem).modal("hide");
             };
 
             //Watch for changes to the modal-visible attribute
-            scope.$watch(attrs.modalShow, function (newValue, oldValue) {
+            scope.$watch(attrs.modalShow, function(newValue, oldValue) {
                 scope.showModal(newValue, attrs.$$element);
             });
 
             //Update the visible value when the dialog is closed through UI actions (Ok, cancel, etc.)
-            $(element).bind("hide.bs.modal", function () {
+            $(element).bind("hide.bs.modal", function() {
                 $parse(attrs.modalShow).assign(scope, false);
-                if (!scope.$$phase && !scope.$root.$$phase)
+                if(!scope.$$phase && !scope.$root.$$phase)
                     scope.$apply();
             });
         }
@@ -97,10 +102,21 @@ app.controller('MyController', ['$scope', 'cow', function($scope, cow) {
 app.factory('cow', ['$timeout', function($timeout) {
     return {
         say: function() {
-            alert ('Moooo!');
+            alert('Moooo!');
         },
         makeVoice: function() {
             $timeout(this.say, 1000);
         }
     };
 }]);
+// app.factory('DownloadJson', ['$scope', function($scope) {
+//
+//     $http.get("/json/test.json")
+//         .then(function(response) {
+//             $scope.json = response.data;
+//         });
+//
+//     // $scope.download = function() {
+//     //
+//     // }
+// }]);
